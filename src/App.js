4b2f2132d4319f2a8e8fad18db6e8ac48cc120a9
Lynda Santiago <lyntechi@gmail.com>
2020-09-27 18:02:47 -0400

    create product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import data from "./data.json";
+import Products from './components/Products.jsx'
 function App() {
+  const [productData, setProductData] = useState({
+    products: data.products,
+    size: "",
+    sort: "",
+  });
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <div className="content">
+          <div className="main"><Products productData={productData.products}/></div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
       <footer>All right is reserved.</footer>
     </div>
   );
---
+++ src/components/Products.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import formatCurrency from '../utils/formatCurrency';
+
+export default function Products(props) {
+  const { productData } = props;
+  return (
+    <div>
+      <ul className="products">
+        {productData.map((item) => {
+          return (
+            <>
+              <li key={item.id}>
+                <div className="product">
+                  <div className="titles">
+                    {" "}
+                    <a href={"#" + item._id}>
+                      <img src={item.image} alt={item.title}></img>
+                      <p>{item.title}</p>
+                    </a>
+                  </div>
+                  <div className="product-price">
+                    <div>{formatCurrency(item.price)}</div>
+                    <button className="button primary">Add To Cart</button>
+                  </div>
+                </div>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
