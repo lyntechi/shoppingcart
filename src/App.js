768dd5bb48ec4236197aae46913cf2a6ce3dddb8
Lynda Santiago <lyntechi@gmail.com>
2020-09-27 15:58:05 -0400

    got rid of extra line spacing in code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
-   <div className="grid-container">
-     <header>
-       <a href="/">React Shopping Cart</a>
-     </header>
-     <main>
-       Product List
-     </main>
-     <footer>
-       All right is reserved.
-     </footer>
-   </div>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
+    </div>
   );
 }
 
